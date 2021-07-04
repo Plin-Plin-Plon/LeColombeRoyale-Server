fe952be6070c0b22c829d37254f12166cf29362a
PauloSTO <paulosto@live.com>
2021-07-04 18:00:48 -0300

    Initial commit using SpringBoot Initializr

---
+++ src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/LecolomberoyaleServerApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.grupo01.lecolomberoyaleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecolomberoyaleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecolomberoyaleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unesp/rc/grupo01/lecolomberoyaleserver/LecolomberoyaleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.grupo01.lecolomberoyaleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecolomberoyaleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
