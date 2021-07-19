8f8ddc2ff7959d488b6843346dd7e416a8897d84
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-07-19 16:11:01 -0300

    feat: Add JWT Security configuration and validation

--- src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/LecolomberoyaleServerApplication.java
+++ src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/LecolomberoyaleServerApplication.java
@@ -1,13 +1,34 @@
 package br.unesp.rc.grupo01.lecolomberoyaleserver;
 
+import br.unesp.rc.grupo01.lecolomberoyaleserver.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class LecolomberoyaleServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LecolomberoyaleServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LecolomberoyaleServerApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/acesso").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
 
 }
