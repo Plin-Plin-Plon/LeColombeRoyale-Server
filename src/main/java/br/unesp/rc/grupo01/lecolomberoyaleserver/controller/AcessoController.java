ae72850e15f237c19aadd9dd9d22e22e7285a0a3
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-07-19 16:10:28 -0300

    feat: Add AcessoController

---
+++ src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/controller/AcessoController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.rc.grupo01.lecolomberoyaleserver.controller;
+
+import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.Acesso;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author paulo
+ */
+@RestController
+public class AcessoController {
+
+    @PostMapping("acesso")
+    public Acesso login(@RequestParam("usuario") String usuario, @RequestParam("senha") String senha) {
+
+        String token = getJWTToken(usuario);
+        Acesso acesso = new Acesso();
+        acesso.setUsuario(usuario);
+        acesso.setToken(token);
+        return acesso;
+
+    }
+
+    private String getJWTToken(String usuario) {
+        String senha = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("tutorialJWT")
+                .setSubject(usuario)
+                .claim("authorities", grantedAuthorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS512, senha.getBytes()).compact();
+
+        return "Bearer " + token;
+    }
+
+}
