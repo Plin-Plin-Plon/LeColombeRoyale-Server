bc808d67dcbf9d227731089a0798b2a08b6cb73b
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-07-21 22:51:51 -0300

    refactor: Add @Embeddable and @Columns properties to Acesso class to integrate with Pessoa class

--- src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/entity/Acesso.java
+++ src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/entity/Acesso.java
@@ -5,6 +5,9 @@
  */
 package br.unesp.rc.grupo01.lecolomberoyaleserver.entity;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,13 +17,17 @@ import lombok.ToString;
  *
  * @author paulo
  */
+@Embeddable
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
-public class Acesso {
+public class Acesso implements Serializable {
 
+    @Column(name = "acesso_usuario")
     private String usuario;
+    
+    @Column(name = "acesso_senha")
     private String senha;
     private String token;
 
