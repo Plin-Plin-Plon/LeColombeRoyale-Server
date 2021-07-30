35b338a9e52a098ef65cbbbc7ee9c7b822dbe556
PauloSTO <paulosto@live.com>
2021-07-30 04:55:49 -0300

    dev: Update AcessoController with new security implementations and roles setters

--- src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/controller/AcessoController.java
+++ src/main/java/br/unesp/rc/grupo01/lecolomberoyaleserver/controller/AcessoController.java
@@ -10,23 +10,27 @@ import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.Contato;
 import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.Endereco;
 import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.Funcionario;
 import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.Hospede;
+import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.Role;
+import br.unesp.rc.grupo01.lecolomberoyaleserver.entity.AuthToken;
 import br.unesp.rc.grupo01.lecolomberoyaleserver.service.FuncionarioService;
 import br.unesp.rc.grupo01.lecolomberoyaleserver.service.HospedeService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.security.Key;
+import br.unesp.rc.grupo01.lecolomberoyaleserver.security.TokenProvider;
+import br.unesp.rc.grupo01.lecolomberoyaleserver.service.RoleService;
 import java.util.ArrayList;
-
-import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
-import javax.crypto.spec.SecretKeySpec;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,33 +45,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class AcessoController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private BCryptPasswordEncoder bcryptEncoder;
+
+    @Autowired
     private FuncionarioService funcionarioService;
 
     @Autowired
     private HospedeService hospedeService;
 
     @PostMapping("login")
-    public ResponseEntity login(@RequestBody Acesso data) {
-        String token = getJWTToken(data.getUsuario());
+    public ResponseEntity<?> generateToken(@RequestBody Acesso acesso) throws AuthenticationException {
 
-        Acesso acesso = new Acesso();
-        acesso.setUsuario(data.getUsuario());
-        acesso.setSenha(data.getSenha());
-        acesso.setToken(token);
-
-        return ResponseEntity.status(HttpStatus.OK).body(acesso);
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        acesso.getUsuario(),
+                        acesso.getSenha()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = jwtTokenUtil.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
     }
 
     @PostMapping("register")
     public ResponseEntity register(@RequestBody Hospede data) {
         Hospede hospede = new Hospede();
         List<Endereco> enderecos = new ArrayList<>();
-        Acesso acesso = new Acesso();
         Contato contato = new Contato();
 
         enderecos = data.getEndereco();
-        acesso = data.getAcesso();
         contato = data.getContato();
+
+        hospede.setUsuario(data.getUsuario());
+        hospede.setSenha(bcryptEncoder.encode(data.getSenha()));
 
         hospede.setNome(data.getNome());
         hospede.setCpf(data.getCpf());
@@ -77,14 +96,16 @@ public class AcessoController {
             hospede.setEndereco(endereco);
         }
 
-        hospede.setAcesso(acesso);
         hospede.setContato(contato);
+
+        Role role = roleService.findByName("USER");
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(role);
+        
+        hospede.setRoles(roleSet);
 
         hospede = hospedeService.save(hospede);
 
-        /* TODO
-            Linkar com a role correspondente
-         */
         return ResponseEntity.status(HttpStatus.OK).body(hospede);
     }
 
@@ -92,11 +113,12 @@ public class AcessoController {
     public ResponseEntity register(@RequestBody Funcionario data) {
         Funcionario funcionario = new Funcionario();
         List<Endereco> enderecos = new ArrayList<>();
-        Acesso acesso = new Acesso();
         Contato contato = new Contato();
 
+        funcionario.setUsuario(data.getUsuario());
+        funcionario.setSenha(bcryptEncoder.encode(data.getSenha()));
+
         enderecos = data.getEndereco();
-        acesso = data.getAcesso();
         contato = data.getContato();
 
         funcionario.setNome(data.getNome());
@@ -108,33 +130,38 @@ public class AcessoController {
             funcionario.setEndereco(endereco);
         }
 
-        funcionario.setAcesso(acesso);
         funcionario.setContato(contato);
+
+        Role role = roleService.findByName("USER");
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(role);
         
+        role = roleService.findByName("MOD");
+        roleSet.add(role);
+        
+        funcionario.setRoles(roleSet);
+
         funcionario = funcionarioService.save(funcionario);
 
-        /* TODO
-            Linkar com a role correspondente
-         */
         return ResponseEntity.status(HttpStatus.OK).body(funcionario);
     }
 
-    private String getJWTToken(String usuario) {
-        String senha = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-        List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
-        Key key = new SecretKeySpec(senha.getBytes(), 0, senha.getBytes().length, SignatureAlgorithm.HS512.getJcaName());
-
-        String token = Jwts
-                .builder()
-                .setSubject(usuario)
-                .claim("authorities", grantedAuthorities.stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .collect(Collectors.toList()))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
-                .signWith(key, SignatureAlgorithm.HS512)
-                .compact();
-
-        return "Bearer " + token;
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("adminping")
+    public String adminPing() {
+        return "O Admnistrativo é cheiroso";
     }
+
+    @PreAuthorize("hasRole('MOD')")
+    @GetMapping("modping")
+    public String modPing() {
+        return "O Moderador é viado";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("userping")
+    public String userPing() {
+        return "Qualquer plebe consegue ler isso";
+    }
+
 }
